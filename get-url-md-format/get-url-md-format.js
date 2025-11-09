05720e832402505691de78f224b10020150fd9d5
namasucd <n.masu0320@gmail.com>
2025-11-09 15:03:10 +0900

    md向けURL取得追加

---
+++ get-url-md-format/get-url-md-format.js
@@ -0,0 +1,27 @@
+javascript:(
+	function copyToClipBoard() {
+		let title = document.title;
+		let href = document.location.href;
+		let mdlinkText = '[' + title + ']' + '(' + getCanonical() + ')';
+		let textBox = document.createElement("textarea");
+		textBox.setAttribute("id", "target");
+		textBox.setAttribute("type", "hidden");
+		textBox.textContent = mdlinkText;
+		document.body.appendChild(textBox);
+
+		textBox.select();
+		document.execCommand('copy');
+		document.body.removeChild(textBox);
+
+		function getCanonical() {
+			try {
+				return document.querySelector('link[rel="canonical"]').href
+			} catch {
+				return document.location.href;
+			}
+		}
+	}
+)()
+
+// ワンライナー
+// javascript:(function copyToClipBoard(){let title=document.title;let href=document.location.href;let mdlinkText='['+title+']'+'('+getCanonical()+')';let textBox=document.createElement("textarea");textBox.setAttribute("id","target");textBox.setAttribute("type","hidden");textBox.textContent=mdlinkText;document.body.appendChild(textBox);textBox.select();document.execCommand('copy');document.body.removeChild(textBox);function getCanonical(){try{return document.querySelector('link[rel="canonical"]').href}catch{return document.location.href;}}})()
